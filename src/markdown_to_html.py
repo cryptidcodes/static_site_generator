3d2df90881e94a92ac9b2aae544372ba22e9a1d3
cryptidcodes <jrkenny5@gmail.com>
2024-08-13 14:49:41 -0700

    Removed test function - should be a unit test

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -41,19 +41,3 @@ def markdown_to_html_node(markdown):
             nodelist.append(ParentNode("p", paragraph))
 
     return ParentNode("div", nodelist)
-
-def test():
-    markdown_to_html_node(
-        """# The Unparalleled Majesty of "The Lord of the Rings"
-
-[Back Home](/)
-
-![LOTR image artistmonkeys](/images/rivendell.png)
-
-> "I cordially dislike allegory in all its manifestations, and always have done so since I grew old and wary enough to detect its presence.
-> I much prefer history, true or feigned, with its varied applicability to the thought and experience of readers.
-> I think that many confuse 'applicability' with 'allegory'; but the one resides in the freedom of the reader, and the other in the purposed domination of the author."
-
-In the annals of fantasy literature and the broader realm of creative world-building, few sagas can rival the intricate tapestry woven by J.R.R. Tolkien in *The Lord of the Rings*. You can find the [wiki here](https://lotr.fandom.com/wiki/Main_Page)."""
-    )
-test()
